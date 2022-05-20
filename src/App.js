2c225160a3eac2d8b3c2dd7079185a39c33f62b9
necmigunduz <necmigunduz@gmail.com>
2022-05-21 00:35:32 +0300

    Fix toggle

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ window.onload = function () {
   const togglePassword = document.querySelector("#togglePassword");
   const password = document.querySelector("#id_password");
   togglePassword.addEventListener("click", function (e) {
-    // toggle the type attribute
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
-    // toggle the eye slash icon
     this.classList.toggle("fa-eye-slash");
   });
 };
